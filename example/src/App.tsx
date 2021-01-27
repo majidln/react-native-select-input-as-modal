058943244e524be87b34cb2e0e871abead5685a1
majidln <majidlotfinia12@gmail.com>
2021-01-27 15:48:36 +0330

    feat(): add some props to select like title etc

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
 import { ModalSelectInput } from 'react-native-select-input';
 
 export default function App() {
+  const [visibleSelect, setVisibleSelect] = React.useState(false);
+  const [selected, setSelected] = React.useState();
   const data = [
     {
       value: '1',
@@ -16,7 +18,19 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <ModalSelectInput data={data} />
+      <TouchableOpacity onPress={() => setVisibleSelect(!visibleSelect)}>
+        <Text>Select data</Text>
+      </TouchableOpacity>
+      {selected && <Text>{JSON.stringify(selected)}</Text>}
+      <ModalSelectInput
+        data={data}
+        isVisible={visibleSelect}
+        onClose={() => setVisibleSelect(false)}
+        label="Select Your data"
+        selected={selected}
+        onSelect={setSelected}
+        returnObject={true}
+      />
     </View>
   );
 }
