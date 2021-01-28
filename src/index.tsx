dfd6250b799a7df2e8fc0867107b51b3f884706a
majidln <majidlotfinia12@gmail.com>
2021-01-28 23:39:28 +0330

    feat(): update export in index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,3 @@
 import ModalSelectInput from './ModalSelectInput';
 
-export { ModalSelectInput };
-
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
+export default ModalSelectInput;
