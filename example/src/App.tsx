1b5e21c590d7ecc582f75eb50fb99b43b4ecd1fd
majidln <majidlotfinia12@gmail.com>
2021-01-30 18:06:41 +0330

    refactor: change package name to react-native-select-input-as-modal

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { StyleSheet, View, TouchableOpacity, Text } from 'react-native';
-import ModalSelectInput from 'react-native-select-input';
+import ModalSelectInput from 'react-native-select-input-as-modal';
 
 export default function App() {
   const [visibleSelect, setVisibleSelect] = React.useState(false);
